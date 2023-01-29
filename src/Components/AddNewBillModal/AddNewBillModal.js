17b9a95584f3064d28c47f6231ed2e8e127775d6
Mezan2002 <mezanurr009@gmail.com>
2023-01-29 20:56:34 +0600

    form validation added

--- src/Components/AddNewBillModal/AddNewBillModal.js
+++ src/Components/AddNewBillModal/AddNewBillModal.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const AddNewBillModal = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const formSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div>
       <div>
@@ -14,54 +23,83 @@ const AddNewBillModal = () => {
               ✕
             </label>
             <h3 className="text-lg text-center font-bold">Add a New Bill</h3>
-            <form>
+            <form onSubmit={handleSubmit(formSubmit)}>
               <div className="form-control w-full">
                 <label className="label">
                   <span className="label-text">Full Name</span>
                 </label>
                 <input
+                  {...register("fullName", { required: true })}
                   type="text"
-                  name="fullName"
                   placeholder=""
                   className="input input-bordered w-full"
-                  required
                 />
+                {errors.fullName?.type === "required" && (
+                  <p className="text-red-600 mt-2" role="alert">
+                    Full Name is Required!
+                  </p>
+                )}
               </div>
               <div className="form-control w-full">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
                 <input
-                  type="text"
-                  name="fullName"
+                  {...register("email", { required: true })}
+                  type="email"
                   placeholder=""
                   className="input input-bordered w-full"
-                  required
                 />
+                {errors.fullName?.type === "required" && (
+                  <p className="text-red-600 mt-2" role="alert">
+                    Email is Required!
+                  </p>
+                )}
               </div>
               <div className="form-control w-full">
                 <label className="label">
-                  <span className="label-text">Phone</span>
+                  <span className="label-text">Phone Number</span>
                 </label>
                 <input
-                  type="text"
-                  name="fullName"
+                  {...register("phone", {
+                    required: true,
+                    maxLength: {
+                      value: 11,
+                      message: "Phone number will be 11 digit",
+                    },
+                    minLength: {
+                      value: 11,
+                      message: "Phone number will be 11 digit",
+                    },
+                  })}
+                  type="number"
                   placeholder=""
                   className="input input-bordered w-full"
-                  required
                 />
+                {errors.phone?.type === "required" && (
+                  <p className="text-red-600 mt-2" role="alert">
+                    Phone Number is Required!
+                  </p>
+                )}
+                {errors?.phone?.message && (
+                  <p className="text-red-600 mt-2">{errors.phone.message}</p>
+                )}
               </div>
               <div className="form-control w-full">
                 <label className="label">
                   <span className="label-text">Payable Amount</span>
                 </label>
                 <input
-                  type="text"
-                  name="fullName"
+                  {...register("payableAmount", { required: true })}
+                  type="number"
                   placeholder=""
                   className="input input-bordered w-full"
-                  required
                 />
+                {errors.fullName?.type === "required" && (
+                  <p className="text-red-600 mt-2" role="alert">
+                    Payable Amount is Required!
+                  </p>
+                )}
               </div>
               <input
                 type="submit"
