5ba652212210afaa085c5e67503316a57660f39c
Mezan2002 <mezanurr009@gmail.com>
2023-01-30 00:32:05 +0600

    edit billing data modal added

--- src/Components/Billings/Billings.js
+++ src/Components/Billings/Billings.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import BillingsTable from "../BillingsTable/BillingsTable";
+import EditBillDetailsModal from "../EditBillDetailsModal/EditBillDetailsModal";
 import Pagination from "../Pagination/Pagination";
 import TopBar from "../TopBar/TopBar";
 
 const Billings = () => {
+  const [modalPrevData, setModalPrevData] = useState([]);
+  const [modalToggle, setModalToggle] = useState(true);
+  const [editModalData, setEditModalData] = useState([]);
   return (
     <div className="mx-24">
       <TopBar></TopBar>
-      <BillingsTable></BillingsTable>
+      <BillingsTable
+        setModalToggle={setModalToggle}
+        setModalPrevData={setModalPrevData}
+      ></BillingsTable>
+      <EditBillDetailsModal
+        modalPrevData={modalPrevData}
+      ></EditBillDetailsModal>
       <Pagination></Pagination>
     </div>
   );
